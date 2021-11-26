5daf2c170a0cc97d150aee1bc180ff26d1ac7a41
fapb92 <69609369+fapb92@users.noreply.github.com>
2021-11-26 08:58:13 -0500

    Se centra el formulario

--- src/componentes/formulario/Registro.jsx
+++ src/componentes/formulario/Registro.jsx
@@ -3,58 +3,60 @@ import '../../css/Registro/registro.css'
 
 export const Registro = () => {
     return (
-        <div className="body-reg">
-            <h1>Registro nuevo usuario</h1>
-            <form className="form-control">
-                <div className="form-label-input">
-                    <label>Nombres</label>
-                    <input type="text" />
-                </div>
+        <div className="center-body">
+            <div className="body-reg">
+                <h1>Registro nuevo usuario</h1>
+                <form className="form-control">
+                    <div className="form-label-input">
+                        <label>Nombres</label>
+                        <input type="text" />
+                    </div>
 
-                <div className="form-label-input">
-                    <label >Apellidos</label>
-                    <input type="text" />
-                </div>
+                    <div className="form-label-input">
+                        <label >Apellidos</label>
+                        <input type="text" />
+                    </div>
 
-                <div className="form-label-input">
-                    <label >Correo</label>
-                    <input type='email' />
-                </div>
+                    <div className="form-label-input">
+                        <label >Correo</label>
+                        <input type='email' />
+                    </div>
 
-                <div className="form-label-input">
-                    <label >Contraseña</label>
-                    <input type="password" />
-                </div>
+                    <div className="form-label-input">
+                        <label >Contraseña</label>
+                        <input type="password" />
+                    </div>
 
-                <div className="form-label-input">
-                    <label >Celular</label>
-                    <input type="text" />
-                </div>
+                    <div className="form-label-input">
+                        <label >Celular</label>
+                        <input type="text" />
+                    </div>
 
-                <div className="form-label-input">
-                    <label >Identificación</label>
-                    <input type="text" />
-                </div>
+                    <div className="form-label-input">
+                        <label >Identificación</label>
+                        <input type="text" />
+                    </div>
 
-                <div className="form-label-input">
-                    <label >Rol</label>
-                    <select name="rol">
-                        <option defaultChecked>Seleccione un cargo</option>
-                        <option value="veterinario">Veterinario</option>
-                        <option value="gestor">Gestor de finca</option>
-                    </select>
-                </div>
+                    <div className="form-label-input">
+                        <label >Rol</label>
+                        <select name="rol">
+                            <option defaultChecked>Seleccione un cargo</option>
+                            <option value="veterinario">Veterinario</option>
+                            <option value="gestor">Gestor de finca</option>
+                        </select>
+                    </div>
 
-                <div className="form-label-input">
-                    <label >Codigo finca</label>
-                    <input type="text" />
-                </div>
+                    <div className="form-label-input">
+                        <label >Codigo finca</label>
+                        <input type="text" />
+                    </div>
 
-                <div className="btn-reg">
-                    <button>Registrarse</button>
-                </div>
+                    <div className="btn-reg">
+                        <button>Registrarse</button>
+                    </div>
 
-            </form>
+                </form>
+            </div>
         </div>
     )
 }
