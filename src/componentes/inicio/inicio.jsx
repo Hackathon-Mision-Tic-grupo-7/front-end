d1cfbd0ce76a4afcc1271284609b068dbf61134e
Paula Andrea Trejos <65204309+patrejos@users.noreply.github.com>
2021-11-26 23:32:32 -0500

    agregar libreria de dialogo en inicio

--- src/componentes/inicio/inicio.jsx
+++ src/componentes/inicio/inicio.jsx
@@ -1,6 +1,10 @@
-import React, {useEffect} from 'react'
+import { Dialog} from '@material-ui/core';
+import React, {useEffect,  useState} from 'react'
+import { NavLink } from 'react-router-dom'
 import '../../css/inicio/inicio.css'
 const Inicio = () => {
+    const [abrir, setabrir] = useState(false)
+
     return (
         <div className = "otra">
             <span>
@@ -12,7 +16,7 @@ const Inicio = () => {
                 La identificación oportuna de enfermedades siempre ha sido un problema para el ganadero, es por esto que se hace necesario hacer seguimiento a los signos vitales  de los individuos y de esa forma poder determinar si se encuentran en buen estado de salud o en el caso de las hembras, determinar si están en celo. El aumento de temperatura, por ejemplo, se puede dar por parásitos, presencia de enfermedades infecto-contagiosas, intoxicaciones, entre otros.
                 </p>
                 <p>
-                    Es entonces este el aplicativo que le permite solucionar ese problema, pues podrá monitorear el estado de sus animales y tendra un contacto directo con el veterinario de confianza.
+                 Es entonces este el aplicativo que le permite solucionar ese problema, pues podrá monitorear el estado de sus animales y tendra un contacto directo con el veterinario de confianza.
                 </p>
             </div>
             <div className="iniciosesion">
@@ -28,12 +32,33 @@ const Inicio = () => {
                     </div>
                     <div >
                         <button className="btn">Inciar sesion</button>
+                        </div>
+                        <div>
                         <span className="aun">¿Aun no tienes cuenta?,</span>
-                        <button className="registrate"> Registrate</button>
+                        <button className="registrate" type="button" onClick={()=>setabrir(true)}> Registrate</button>
+                <Dialog  open={abrir}>
+                <div className="dialog">
+                    <button className="btn-dialog">
+                        <NavLink to={'/registroadmin'}>
+                     Registrarse como administrador
+                     </NavLink>
+                    </button>
+                <button className="btn-dialog">
+                    <NavLink to={'/registro'}>
+                     Registrarse como gestor
+                    </NavLink> 
+                </button>
+                <button className = "btn-dialog" onClick={()=>{setabrir(false)}}>
+                    Cerrar Dialogo
+                </button>
+                </div>
+            </Dialog>
+
                     </div>
                 </form>
 
             </div>
+            
         </div>           
         </div>
 
--- src/componentes/resumen/Resumen.jsx
+++ src/componentes/resumen/Resumen.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { BarraNavegacion } from '../inicioGeneral/BarraNavegacion'
+import '../../css/resumen/resumen.css'
+import { Line } from 'react-chartjs-2'
 
 export const Resumen = () => {
+
+
+
     return (
         <div className= 'body-list-page center-page'>
             <BarraNavegacion/>
-            <div>
-                <h2>Resumen</h2>
+            <div className="resumen">
+                <h2>Resumen General</h2>                 
             </div>
+                <div className="wrapper">
+                    <div className="one"> 
+                        <h2> Promedio diario animales de la finca</h2>
+                    </div>
+                    <div className="two">dos</div>
+                    <div className="tres">tres</div>
+                    <div className="cuatro">cuatro</div>
+                </div>
+
         </div>
     )
 }
