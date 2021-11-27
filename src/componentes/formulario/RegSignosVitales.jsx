c3fafc4840df156b7628fb535442e4a03b416086
fapb92 <69609369+fapb92@users.noreply.github.com>
2021-11-27 09:38:31 -0500

    Se hace ajustes a las paginas

--- src/componentes/formulario/RegSignosVitales.jsx
+++ src/componentes/formulario/RegSignosVitales.jsx
@@ -1,42 +1,46 @@
 import React from 'react'
 import "../../css/Registro/crearAnimalSignosVitales.css"
+import { BarraNavegacion } from '../inicioGeneral/BarraNavegacion'
 
 export const RegSignosVitales = () => {
     return (
-        <div className="center-body">
-            <div className="body-reg">
-                <h1>Agregar signos vitales</h1>
-                <form className="form-control-bovino">
-                     <div className="form-label-input-bovino">
-                        <label >Fecha y hora</label>
-                        <input type="datetime-local" />
-                    </div>
-                    
-                    <div className="form-label-input-bovino">
-                        <label>Numero Identificaión (ID)</label>
-                        <input type="text" />
-                    </div>
+        <>
+            <BarraNavegacion />
+            <div className="center-body">
+                <div className="body-reg">
+                    <h1>Agregar signos vitales</h1>
+                    <form className="form-control-bovino">
+                        <div className="form-label-input-bovino">
+                            <label >Fecha y hora</label>
+                            <input type="datetime-local" />
+                        </div>
 
-                    <div className="form-label-input-bovino">
-                        <label >Temperatura (°C)</label>
-                        <input type='number' />
-                    </div>
+                        <div className="form-label-input-bovino">
+                            <label>Numero Identificaión (ID)</label>
+                            <input type="text" />
+                        </div>
 
-                    <div className="form-label-input-bovino">
-                        <label >Frecuecia cardiaca (Ipm)</label>
-                        <input type='number' />
-                    </div>
+                        <div className="form-label-input-bovino">
+                            <label >Temperatura (°C)</label>
+                            <input type='number' />
+                        </div>
 
-                    <div className="form-label-input-bovino">
-                        <label >Frecuecia respiratoria (rpm)</label>
-                        <input type='number' />
-                    </div>
-                    <div className="btn-reg">
-                        <button>Agregar bovino</button>
-                    </div>
+                        <div className="form-label-input-bovino">
+                            <label >Frecuecia cardiaca (Ipm)</label>
+                            <input type='number' />
+                        </div>
 
-                </form>
+                        <div className="form-label-input-bovino">
+                            <label >Frecuecia respiratoria (rpm)</label>
+                            <input type='number' />
+                        </div>
+                        <div className="btn-reg">
+                            <button>Agregar bovino</button>
+                        </div>
+
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
--- src/componentes/inicioGeneral/ListaSolicitudes.jsx
+++ src/componentes/inicioGeneral/ListaSolicitudes.jsx
@@ -5,32 +5,34 @@ import { BarraNavegacion } from './BarraNavegacion'
 
 export const ListaSolicitudes = () => {
     return (
-        <div className="body-list-page center-page">
-            <BarraNavegacion/>
-            <div>
-                <h1>Lista de solicitudes</h1>
-                <table className="table-animals">
-                    <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Nombre</th>
-                            <th>Estado</th>
-                            <th>Cargo</th>
-                            <th>Aceptar / Rechazar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1234</td>
-                            <td>15/11/2015</td>
-                            <td>37</td>
-                            <td><button className="list-btns-ver">Ver mas</button></td>
-                            <td><button className="list-btns-check"> 	
-&#10003;</button> <button className="list-btns-x">x</button></td>
-                        </tr>
-                    </tbody>
-                </table>
+        <>
+            <BarraNavegacion />
+            <div className="body-list-page center-page">
+                <div>
+                    <h1>Lista de solicitudes</h1>
+                    <table className="table-animals">
+                        <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Nombre</th>
+                                <th>Estado</th>
+                                <th>Cargo</th>
+                                <th>Aceptar / Rechazar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1234</td>
+                                <td>15/11/2015</td>
+                                <td>37</td>
+                                <td><button className="list-btns-ver">Ver mas</button></td>
+                                <td><button className="list-btns-check">
+                                    &#10003;</button> <button className="list-btns-x">x</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
