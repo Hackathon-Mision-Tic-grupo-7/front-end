c61c1a2a071ea0e37c9fff5eb914f928edae1a78
fapb92 <69609369+fapb92@users.noreply.github.com>
2021-11-29 16:17:36 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-     <App />
     <Auth0Provider
     domain="dev-7wmznvx4.us.auth0.com"
     clientId="Vuq4mVNpzyOHa6mclPD4iAWUYrogh6mq"
-    redirectUri="https://hackathon-frontend1.herokuapp.com/">
+    redirectUri={window.location.origin}>
+      <App />
     </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
